ff21fdf12cd7dff3eec077be78a62148fd10f9e7
liujie045 <liujie045.china@outlook.com>
2017-03-22 13:51:06 +0800

    最新代码提交

--- app/src/main/java/com/handy/base/app/MainActivity.java
+++ app/src/main/java/com/handy/base/app/MainActivity.java
@@ -1,7 +1,9 @@
-package com.handy.base;
+package com.handy.base.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.handy.base.R;
 
 public class MainActivity extends AppCompatActivity {
 
