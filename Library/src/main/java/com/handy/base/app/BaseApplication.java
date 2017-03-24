977cd213adb49c68b144e9c03a8823c5780b6eae
liujie045 <liujie045.china@outlook.com>
2017-03-24 10:05:54 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -2,19 +2,27 @@ package com.handy.base.app;
 
 import android.app.Application;
 
+import com.handy.base.utils.CrashUtils;
 import com.handy.base.utils.HandyBaseUtils;
+import com.handy.base.utils.LogUtils;
 
 /**
  * Application基本类
  * Created by LiuJie on 2016/5/7.
  */
 public abstract class BaseApplication extends Application {
+    public boolean isInitHandyBaseUtils = true;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         try {
-            HandyBaseUtils.getInstance().registerUtils(getApplicationContext());
+            if (isInitHandyBaseUtils) {
+                HandyBaseUtils.getInstance().registerUtils(getApplicationContext());
+                CrashUtils.getInstance().initCrashUtils();
+                LogUtils.getInstance().initLogUtils(true, true, 'v', "HandyBase_Log");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
