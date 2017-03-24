410b48b93bfadeff06d8420be8f0d7e0a8a841e3
liujie045 <liujie045.china@outlook.com>
2017-03-24 10:06:04 +0800

    Handy Commit

---
+++ app/src/main/java/com/handy/base/app/MyBaseApplication.java
@@ -0,0 +1,13 @@
+package com.handy.base.app;
+
+/**
+ * Created by LiuJie on 2017/3/24.
+ */
+
+public class MyBaseApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: 2017/3/24 手动初始化工具类
+    }
+}
