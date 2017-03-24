48b8843b7d7ed3ef4e9b204f9e6a227698846f12
liujie045 <liujie045.china@outlook.com>
2017-03-24 11:20:39 +0800

    Handy Commit

--- app/src/main/java/com/handy/base/app/MyBaseApplication.java
+++ app/src/main/java/com/handy/base/app/MyBaseApplication.java
@@ -1,10 +1,26 @@
 package com.handy.base.app;
 
+import android.Manifest;
+
+import com.handy.base.utils.PermissionsUtils;
+
+import java.util.ArrayList;
+
 /**
  * Created by LiuJie on 2017/3/24.
  */
 
 public class MyBaseApplication extends BaseApplication {
+    {
+        PermissionsUtils.getInstance().Permissions = new ArrayList<String>() {{
+            add(Manifest.permission.INTERNET);
+            add(Manifest.permission.READ_PHONE_STATE);
+            add(Manifest.permission.ACCESS_WIFI_STATE);
+            add(Manifest.permission.ACCESS_NETWORK_STATE);
+            add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }};
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
