60b2653cf4106420f65c87cd1445c41b6dddddef
liujie045 <liujie045.china@outlook.com>
2017-03-29 10:56:43 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/utils/PermissionsUtils.java
+++ Library/src/main/java/com/handy/base/utils/PermissionsUtils.java
@@ -40,7 +40,9 @@ public class PermissionsUtils {
     private void initPermissions() {
         Permissions = new ArrayList<String>() {{
             add(Manifest.permission.INTERNET);
+            add(Manifest.permission.RECORD_AUDIO);
             add(Manifest.permission.READ_PHONE_STATE);
+            add(Manifest.permission.ACCESS_WIFI_STATE);
             add(Manifest.permission.ACCESS_NETWORK_STATE);
             add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }};
