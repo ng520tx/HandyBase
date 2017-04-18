24b669d6aa306adf4102d68adea86e944a3d4e1c
liujie045 <liujie045.china@outlook.com>
2017-04-18 16:39:17 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -2,9 +2,9 @@ package com.handy.base.app;
 
 import android.app.Application;
 
+import com.handy.base.utils.AppUtils;
 import com.handy.base.utils.CleanUtils;
 import com.handy.base.utils.CrashUtils;
-import com.handy.base.utils.HandyBaseUtils;
 import com.handy.base.utils.LogUtils;
 import com.pgyersdk.crash.PgyCrashManager;
 
@@ -13,6 +13,7 @@ import com.pgyersdk.crash.PgyCrashManager;
  * Created by LiuJie on 2016/5/7.
  */
 public abstract class BaseApplication extends Application {
+    public boolean isCleanCache = true;
     public boolean isInitHandyBaseUtils = true;
     public boolean isInitPgyCrashManager = false;
 
@@ -22,14 +23,21 @@ public abstract class BaseApplication extends Application {
 
         try {
             if (isInitHandyBaseUtils) {
-                HandyBaseUtils.getInstance().registerUtils(getApplicationContext());
+                if (isCleanCache) { //清空手机内部和外部缓存数据
+                    CleanUtils.getInstance().cleanInternalCache(getApplicationContext());
+                    CleanUtils.getInstance().cleanExternalCache(getApplicationContext());
+                }
 
-                /* 清空手机内部和外部缓存数据 */
-                CleanUtils.getInstance().cleanInternalCache();
-                CleanUtils.getInstance().cleanExternalCache();
+                CrashUtils.getInstance().init(getApplicationContext()); //初始化崩溃捕获工具
 
-                CrashUtils.getInstance().initCrashUtils(); //初始化崩溃捕获工具
-                LogUtils.getInstance().initLogUtils(true, true, 'v', "HandyLog"); //初始化日志输出工具
+                LogUtils.getInstance().initBuilder(getApplicationContext())
+                        .setLogSwitch(AppUtils.getInstance().isAppDebug(getApplicationContext()))// 设置log总开关，默认开
+                        .setGlobalTag("HandyBase")// 设置log全局标签，默认为空
+                        // 当全局标签不为空时，我们输出的log全部为该tag，
+                        // 为空时，如果传入的tag为空那就显示类名，否则显示tag
+                        .setLog2FileSwitch(false)// 打印log时是否存到文件的开关，默认关
+                        .setBorderSwitch(true)// 输出日志是否带边框开关，默认开
+                        .setLogFilter(LogUtils.V);// log过滤器，和logcat过滤器同理，默认Verbose
 
                 LogUtils.getInstance().d("工具类功能已注册");
             }
