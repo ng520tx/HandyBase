5fa62fb86dbc76e1194ec2c80af30c773a91f32d
liujie045 <liujie045.china@outlook.com>
2017-04-18 17:56:55 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseAppApi.java
+++ Library/src/main/java/com/handy/base/app/BaseAppApi.java
@@ -6,60 +6,57 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by LiuJie on 2016/12/26.
- */
-
 class BaseAppApi {
 
     interface BaseAtyApi {
+
+        void initIntentBundle(Bundle intentBundle);
+
         /**
-         * ===================================================================
          * 在onStart方法中被调用，用于初始化界面布局控件
          *
-         * @param savedInstanceState 缓存数据
          */
-        void initActivityView(@Nullable Bundle savedInstanceState);
+        void initHDBView(@Nullable Bundle savedInstanceState);
 
         /**
-         * 在onStart方法中且initActivityView执行完后被调用，用于初始化界面有关数据。
+         * 在onStart方法中且initView执行完后被调用，用于初始化界面有关数据。
          */
-        void initActivityData();
+        void initHDBData();
 
         /**
-         * 在onResume方法中被调用，当Activity调用onResume方法时被调用
+         * 在onResume方法中被调用，当调用onResume方法时被调用
          */
-        void onActivityRefresh();
+        void onHDBRefresh();
 
         /**
          * 在onResume方法中被调用，当界面对用户可见且isRequesting==true时执行此方法，用于可能重复执行的操作。
          */
-        void onActivityRequest();
+        void onHDBRequest();
 
 
         /**
          * 在onStart方法中被调用，用户检查权限
          */
-        void checkActivityPermissions();
+        void checkHDBPermissions();
 
         /**
          * 当权限扫描通过时（全部权限均已开启），执行此方法。若不许执行权限扫描或避免此方法。
          * <pre>
          * 注意：此方法的执行顺序是:
-         * 子类 initActivityData()      super  前
-         * 父类 initActivityData()      方法
-         * 子类 onActivityPermission()  super  前
-         * 父类 onActivityPermission()  方法
-         * 子类 onActivityPermission()  super  后
-         * 子类 initActivityData()      super  后
+         * 子类 initHDBData()      super  前
+         * 父类 initHDBData()      方法
+         * 子类 onPermission()  super  前
+         * 父类 onPermission()  方法
+         * 子类 onPermission()  super  后
+         * 子类 initHDBData()      super  后
          * </pre>
          */
-        void onActivityPermissionSuccess();
+        void onHDBPermissionSuccess();
 
         /**
          * 当权限扫描不通过，且请求被拒绝时执行此方法。若不许执行权限扫描或避免此方法。
          */
-        void onActivityPermissionRejection();
+        void onHDBPermissionRejection();
     }
 
     interface BaseFgmApi {
@@ -67,34 +64,34 @@ class BaseAppApi {
          * ===================================================================
          * 在onCreate方法中被调用，用于初始化界面布局控件
          */
-        View initFragmentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState);
+        View initHDBView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState);
 
         /**
          * 当initView执行完后被调用，用户初始化界面控件UI
          *
-         * @param view               当前Fragment布局View
+         * @param view               当前布局View
          * @param savedInstanceState 缓存数据
          */
-        void onFragmentComplete(View view, @Nullable Bundle savedInstanceState);
+        void onHDBComplete(View view, @Nullable Bundle savedInstanceState);
 
         /**
          * 在onCreate方法中且initView执行完后被调用，用于初始化界面有关数据。
          */
-        void initFragmentData();
+        void initHDBData();
 
         /**
-         * 在onResume方法中被调用，当Fragment调用onResume方法时被调用
+         * 在onResume方法中被调用，当调用onResume方法时被调用
          */
-        void onFragmentRefresh();
+        void onHDBRefresh();
 
         /**
          * 在setUserVisibleHint方法中被调用，当界面对用户可见时被调用
          */
-        void onFragmentVisiable();
+        void onHDBVisiable();
 
         /**
          * 在onResume方法中被调用，当界面对用户可见且isRequesting==true时执行此方法，用于可能重复执行的操作。
          */
-        void onFragmentRequest();
+        void onHDBRequest();
     }
 }
