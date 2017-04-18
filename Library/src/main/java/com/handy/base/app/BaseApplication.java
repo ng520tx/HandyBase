903d4ecf4bd5d398325fcab536364310f46ac372
liujie045 <liujie045.china@outlook.com>
2017-04-18 18:12:59 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -8,18 +8,23 @@ import com.handy.base.utils.LogUtils;
 import com.pgyersdk.crash.PgyCrashManager;
 
 /**
- * Application基本类
- * Created by LiuJie on 2016/5/7.
+ * <pre>
+ *  author: Handy
+ *  blog  : https://github.com/liujie045
+ *  time  : 2017-4-18 10:14:23
+ *  desc  : Application基本类
+ * </pre>
  */
 public abstract class BaseApplication extends Application {
-    public boolean isInitHandyBaseUtils = true;
+    public boolean isInitUtils = true;
     public boolean isInitPgyCrashManager = false;
 
     @Override
     public void onCreate() {
         super.onCreate();
         try {
-            if (isInitHandyBaseUtils) {
+            /* 初始化工具类功能 */
+            if (isInitUtils) {
                 CrashUtils.getInstance().init(getApplicationContext()); //初始化崩溃捕获工具
 
                 LogUtils.getInstance().initBuilder(getApplicationContext())
@@ -28,17 +33,14 @@ public abstract class BaseApplication extends Application {
                         .setLog2FileSwitch(false)// 打印log时是否存到文件的开关，默认关
                         .setBorderSwitch(true)// 输出日志是否带边框开关，默认开
                         .setLogFilter(LogUtils.V);// log过滤器，和logcat过滤器同理，默认Verbose
-
-                LogUtils.getInstance().d("工具类功能已注册");
             }
-
+            /* 初始化蒲公英内测功能 */
             if (isInitPgyCrashManager) {
                 PgyCrashManager.register(getApplicationContext());
                 LogUtils.getInstance().d("蒲公英内测功能已注册");
             }
         } catch (Exception e) {
             e.printStackTrace();
-            LogUtils.getInstance().w(null, "", e);
         }
     }
 
