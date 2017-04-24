565b3e2d1b5efbac7dd7d8f28148a252db50e107
liujie045 <liujie045.china@outlook.com>
2017-04-24 15:05:58 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -29,11 +29,20 @@ public abstract class BaseApplication extends Application {
 
                 if (AppUtils.getInstance().isAppDebug(getApplicationContext())) {
                     LogUtils.getInstance().initBuilder(getApplicationContext())
-                            .setLogSwitch(AppUtils.getInstance().isAppDebug(getApplicationContext()))// 设置log总开关，默认开
-                            .setGlobalTag("")// 设置log全局标签，默认为空，当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
-                            .setLog2FileSwitch(true)// 打印log时是否存到文件的开关，默认关
-                            .setBorderSwitch(false)// 输出日志是否带边框开关，默认开
-                            .setLogFilter(LogUtils.V);// log过滤器，和logcat过滤器同理，默认Verbose
+                            .setLogSwitch(AppUtils.getInstance().isAppDebug(getApplicationContext())) //设置log总开关，默认开
+                            .setGlobalTag("HandyBase") //设置log全局标签，默认为空，当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
+                            .setLog2FileSwitch(true) //打印log时是否存到文件的开关，默认关
+                            .setEncryptSwitch(false) //存到文件的log内容加密，默认关
+                            .setBorderSwitch(true) //输出日志是否带边框开关，默认开
+                            .setLogFilter(LogUtils.V); //log过滤器，和logcat过滤器同理，默认Verbose
+                } else {
+                    LogUtils.getInstance().initBuilder(getApplicationContext())
+                            .setLogSwitch(AppUtils.getInstance().isAppDebug(getApplicationContext())) //设置log总开关，默认开
+                            .setGlobalTag("") //设置log全局标签，默认为空，当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
+                            .setLog2FileSwitch(true) //打印log时是否存到文件的开关，默认关
+                            .setEncryptSwitch(true) //存到文件的log内容加密，默认关
+                            .setBorderSwitch(false) //输出日志是否带边框开关，默认开
+                            .setLogFilter(LogUtils.V); //log过滤器，和logcat过滤器同理，默认Verbose
                 }
             }
             /* 初始化蒲公英内测功能 */
