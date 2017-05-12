2fbda20f3c759f3a6d7a042f848a56e8c501ea4c
liujie045 <liujie045.china@outlook.com>
2017-05-12 16:43:49 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.handy.base.utils.AppUtils;
 import com.handy.base.utils.CrashUtils;
 import com.handy.base.utils.LogUtils;
+import com.handy.base.utils.Utils;
 import com.pgyersdk.crash.PgyCrashManager;
 
 /**
@@ -25,30 +26,23 @@ public abstract class BaseApplication extends Application {
         try {
             /* 初始化工具类功能 */
             if (isInitUtils) {
-                CrashUtils.getInstance().init(getApplicationContext()); //初始化崩溃捕获工具
+                Utils.init(getApplicationContext());
+                CrashUtils.getInstance().init(); //初始化崩溃捕获工具
 
-                if (AppUtils.getInstance().isAppDebug(getApplicationContext())) {
-                    LogUtils.getInstance().initBuilder(getApplicationContext())
-                            .setLogSwitch(AppUtils.getInstance().isAppDebug(getApplicationContext())) //设置log总开关，默认开
-                            .setGlobalTag("HDB") //设置log全局标签，默认为空，当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
-                            .setLog2FileSwitch(true) //打印log时是否存到文件的开关，默认关
-                            .setEncryptSwitch(false) //存到文件的log内容加密，默认关
-                            .setBorderSwitch(true) //输出日志是否带边框开关，默认开
-                            .setLogFilter(LogUtils.V); //log过滤器，和logcat过滤器同理，默认Verbose
-                } else {
-                    LogUtils.getInstance().initBuilder(getApplicationContext())
-                            .setLogSwitch(AppUtils.getInstance().isAppDebug(getApplicationContext())) //设置log总开关，默认开
-                            .setGlobalTag("") //设置log全局标签，默认为空，当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
-                            .setLog2FileSwitch(true) //打印log时是否存到文件的开关，默认关
-                            .setEncryptSwitch(true) //存到文件的log内容加密，默认关
-                            .setBorderSwitch(false) //输出日志是否带边框开关，默认开
-                            .setLogFilter(LogUtils.V); //log过滤器，和logcat过滤器同理，默认Verbose
-                }
+                LogUtils.Builder builder = new LogUtils.Builder()
+                        .setLogSwitch(AppUtils.isAppDebug())// 设置log总开关，默认开
+                        .setGlobalTag(null)// 设置log全局标签，默认为空。当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
+                        .setLogHeadSwitch(true)// 设置log头信息开关，默认为开
+                        .setLog2FileSwitch(AppUtils.isAppDebug())// 打印log时是否存到文件的开关，默认关
+                        .setDir("")// 当自定义路径为空时，写入应用的/cache/log/目录中
+                        .setBorderSwitch(true)// 输出日志是否带边框开关，默认开
+                        .setLogFilter(LogUtils.V);// log过滤器，和logcat过滤器同理，默认Verbose
+                LogUtils.d(builder.toString());
             }
             /* 初始化蒲公英内测功能 */
             if (isInitPgyCrashManager) {
                 PgyCrashManager.register(getApplicationContext());
-                LogUtils.getInstance().d("蒲公英内测功能已注册");
+                LogUtils.d("蒲公英内测功能已注册");
             }
         } catch (Exception e) {
             e.printStackTrace();
