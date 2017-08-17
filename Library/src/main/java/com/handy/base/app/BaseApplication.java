6c0945bc78cab0118bd732a8f64607d57aa84d32
刘杰 <liujie045.china@outlook.com>
2017-08-17 15:12:19 +0800

    第三方接入功能版本更新

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -11,7 +11,7 @@ import com.pgyersdk.crash.PgyCrashManager;
 /**
  * <pre>
  *  author: Handy
- *  blog  : https://github.com/liujie045
+ *  blog  : https://github.com/handy045
  *  time  : 2017-4-18 10:14:23
  *  desc  : Application基本类
  * </pre>
@@ -24,21 +24,24 @@ public abstract class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         try {
-            Utils.init(getApplicationContext());
+            Utils.init(this);
 
             /* 初始化工具类功能 */
             if (isInitUtils) {
-                CrashUtils.getInstance().init(); //初始化崩溃捕获工具
-
-                LogUtils.Builder builder = new LogUtils.Builder()
-                        .setLogSwitch(AppUtils.isAppDebug())// 设置log总开关，默认开
-                        .setGlobalTag(null)// 设置log全局标签，默认为空。当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
+                CrashUtils.init(); //初始化崩溃捕获工具
+                LogUtils.Config config = LogUtils.getConfig()
+                        .setLogSwitch(AppUtils.isAppDebug())// 设置log总开关，包括输出到控制台和文件，默认开
+                        .setConsoleSwitch(AppUtils.isAppDebug())// 设置是否输出到控制台开关，默认开
+                        .setGlobalTag(null)// 设置log全局标签，默认为空
+                        // 当全局标签不为空时，我们输出的log全部为该tag，
+                        // 为空时，如果传入的tag为空那就显示类名，否则显示tag
                         .setLogHeadSwitch(true)// 设置log头信息开关，默认为开
                         .setLog2FileSwitch(AppUtils.isAppDebug())// 打印log时是否存到文件的开关，默认关
                         .setDir("")// 当自定义路径为空时，写入应用的/cache/log/目录中
-                        .setBorderSwitch(true)// 输出日志是否带边框开关，默认开
-                        .setLogFilter(LogUtils.V);// log过滤器，和logcat过滤器同理，默认Verbose
-                LogUtils.d(builder.toString());
+                        .setBorderSwitch(false)// 输出日志是否带边框开关，默认开
+                        .setConsoleFilter(LogUtils.V)// log的控制台过滤器，和logcat过滤器同理，默认Verbose
+                        .setFileFilter(LogUtils.V);// log文件过滤器，和logcat过滤器同理，默认Verbose
+                LogUtils.d(config.toString());
             }
 
             /* 初始化蒲公英内测功能 */
