d570bec7618af183e0dbfaa9b16b1b4269e49773
刘杰 <liujie045.china@outlook.com>
2017-08-17 17:07:39 +0800

    第三方接入功能版本更新

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -37,7 +37,7 @@ public abstract class BaseApplication extends Application {
                         // 为空时，如果传入的tag为空那就显示类名，否则显示tag
                         .setLogHeadSwitch(true)// 设置log头信息开关，默认为开
                         .setLog2FileSwitch(AppUtils.isAppDebug())// 打印log时是否存到文件的开关，默认关
-                        .setDir("")// 当自定义路径为空时，写入应用的/cache/log/目录中
+                        .setDir("HandyBase")// 当自定义路径为空时，写入应用的/cache/log/目录中
                         .setBorderSwitch(false)// 输出日志是否带边框开关，默认开
                         .setConsoleFilter(LogUtils.V)// log的控制台过滤器，和logcat过滤器同理，默认Verbose
                         .setFileFilter(LogUtils.V);// log文件过滤器，和logcat过滤器同理，默认Verbose
