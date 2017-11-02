6fcd30737ffa221448b2b78bae19dda259585b63
刘杰 <liujie2015@boco.com.cn>
2017-11-02 17:56:00 +0800

    增加图片水印的换行能力

--- Library/src/main/java/com/handy/base/utils/PermissionsUtils.java
+++ Library/src/main/java/com/handy/base/utils/PermissionsUtils.java
@@ -35,16 +35,20 @@ public final class PermissionsUtils {
         Permissions = new ArrayList<String>() {{
             add(Manifest.permission.INTERNET);
             add(Manifest.permission.RECORD_AUDIO);
-            add(Manifest.permission.RESTART_PACKAGES);
             add(Manifest.permission.READ_PHONE_STATE);
             add(Manifest.permission.ACCESS_WIFI_STATE);
             add(Manifest.permission.ACCESS_NETWORK_STATE);
+            add(Manifest.permission.VIBRATE);
             add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }};
     }
 
     public static ArrayList<String> getPermissions() {
         return Permissions;
+    }
+
+    public static void setPermissions(ArrayList<String> permissions) {
+        Permissions = permissions;
     }
 
     /**
@@ -59,6 +63,7 @@ public final class PermissionsUtils {
      * add(Manifest.permission.READ_PHONE_STATE);
      * add(Manifest.permission.ACCESS_WIFI_STATE);
      * add(Manifest.permission.ACCESS_NETWORK_STATE);
+     * add(Manifest.permission.VIBRATE);
      * add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
      * </pre>
      *
