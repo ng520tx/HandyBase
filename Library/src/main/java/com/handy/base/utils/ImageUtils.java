c4ad2c1f10b0e0abaeecce7f7b709d05e40803a9
刘杰 <liujie2015@boco.com.cn>
2017-11-02 18:23:07 +0800

    HandyCommit

---
+++ Library/src/main/java/com/handy/base/utils/ImageUtils.java
@@ -0,0 +1,142 @@
+package com.handy.base.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+
+/**
+ * <pre>
+ *  author : LiuJie
+ *  desc : 图片相关工具类
+ *  blog : https://github.com/Handy045
+ *  createtime : 2017/11/2 18:20
+ *  updatetime : 2017/11/2 18:20
+ * </pre>
+ */
+public class ImageUtils {
+    /**
+     * 添加文字水印
+     *
+     * @param src      源图片
+     * @param content  水印文本
+     * @param textSize 水印字体大小
+     * @param color    水印字体颜色
+     * @param x        起始坐标x
+     * @param y        起始坐标y
+     * @return 带有文字水印的图片
+     */
+    public static Bitmap addTextWatermark(final Bitmap src,
+                                          final String content,
+                                          final int textSize,
+                                          final int color,
+                                          final float x,
+                                          final float y) {
+        return addTextWatermark(src, content, textSize, color, x, y, false);
+    }
+
+    /**
+     * 添加文字水印
+     *
+     * @param src       源图片
+     * @param content   水印文本
+     * @param textScale 水印字体比例（字体大小 = 照片高度 / 字体比例）
+     * @param color     水印字体颜色
+     * @param x         起始坐标x
+     * @param y         起始坐标y
+     * @return 带有文字水印的图片
+     */
+    public static Bitmap addTextWatermarkScale(final Bitmap src,
+                                               final String content,
+                                               final int textScale,
+                                               final int color,
+                                               final float x,
+                                               final float y) {
+        return addTextWatermark(src, content, src.getHeight() / textScale, color, x, y, false);
+    }
+
+    /**
+     * 添加文字水印，支持换行
+     *
+     * @param src      源图片
+     * @param content  水印文本
+     * @param textSize 水印字体大小
+     * @param color    水印字体颜色
+     * @param x        起始坐标x
+     * @param y        起始坐标y
+     * @param recycle  是否回收
+     * @return 带有文字水印的图片
+     */
+    public static Bitmap addTextWatermark(final Bitmap src,
+                                          final String content,
+                                          final float textSize,
+                                          final int color,
+                                          final float x,
+                                          final float y,
+                                          final boolean recycle) {
+        if (isEmptyBitmap(src) || content == null) return null;
+        Bitmap ret = src.copy(src.getConfig(), true);
+        TextPaint paint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+        Canvas canvas = new Canvas(ret);
+        paint.setColor(color);
+        paint.setTextSize(textSize);
+        Rect bounds = new Rect();
+        paint.getTextBounds(content, 0, content.length(), bounds);
+        StaticLayout layout = new StaticLayout(content, paint, (int) (ret.getWidth() - x), Layout.Alignment.ALIGN_NORMAL, 1.0F, 0.0F, true);
+        canvas.translate(x, y);
+        layout.draw(canvas);
+        if (recycle && !src.isRecycled()) src.recycle();
+        return ret;
+    }
+
+    /**
+     * 添加图片水印
+     *
+     * @param src       源图片
+     * @param watermark 图片水印
+     * @param x         起始坐标x
+     * @param y         起始坐标y
+     * @param alpha     透明度
+     * @return 带有图片水印的图片
+     */
+    public static Bitmap addImageWatermark(final Bitmap src, final Bitmap watermark, final int x, final int y, final int alpha) {
+        return addImageWatermark(src, watermark, x, y, alpha, false);
+    }
+
+    /**
+     * 添加图片水印
+     *
+     * @param src       源图片
+     * @param watermark 图片水印
+     * @param x         起始坐标x
+     * @param y         起始坐标y
+     * @param alpha     透明度
+     * @param recycle   是否回收
+     * @return 带有图片水印的图片
+     */
+    public static Bitmap addImageWatermark(final Bitmap src, final Bitmap watermark, final int x, final int y, final int alpha, final boolean recycle) {
+        if (isEmptyBitmap(src)) return null;
+        Bitmap ret = src.copy(src.getConfig(), true);
+        if (!isEmptyBitmap(watermark)) {
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            Canvas canvas = new Canvas(ret);
+            paint.setAlpha(alpha);
+            canvas.drawBitmap(watermark, x, y, paint);
+        }
+        if (recycle && !src.isRecycled()) src.recycle();
+        return ret;
+    }
+
+    /**
+     * 判断bitmap对象是否为空
+     *
+     * @param src 源图片
+     * @return {@code true}: 是<br>{@code false}: 否
+     */
+    private static boolean isEmptyBitmap(final Bitmap src) {
+        return src == null || src.getWidth() == 0 || src.getHeight() == 0;
+    }
+}
