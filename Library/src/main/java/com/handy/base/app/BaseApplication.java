b1ac710bf6e0d8cfcc90256f5add48a31f9924b1
刘杰 <liujie2015@boco.com.cn>
2017-11-02 18:04:49 +0800

    增加图片水印的换行能力

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -2,10 +2,12 @@ package com.handy.base.app;
 
 import android.app.Application;
 
-import com.blankj.utilcode.util.CrashUtils;
+import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.LogUtils;
-import com.handy.base.BuildConfig;
+import com.handy.base.utils.androidutilcode.Utils;
 import com.pgyersdk.crash.PgyCrashManager;
+
+import org.litepal.LitePal;
 
 /**
  * <pre>
@@ -16,39 +18,52 @@ import com.pgyersdk.crash.PgyCrashManager;
  * </pre>
  */
 public abstract class BaseApplication extends Application {
+
     public boolean isInitUtils = true;
+    public boolean isUseCuntomCrashUtil = true;
     public boolean isInitPgyCrashManager = false;
+
+    /**
+     * 日志工具配置类
+     */
+    public LogUtils.Config config;
 
     @Override
     public void onCreate() {
         super.onCreate();
         try {
+            Utils.init(getApplicationContext());
+            LitePal.initialize(getApplicationContext());
+
+             /* 初始化崩溃捕获工具 */
+            if (isUseCuntomCrashUtil) {
+                com.handy.base.utils.CrashUtils.init();
+            } else {
+                com.blankj.utilcode.util.CrashUtils.init();
+            }
 
             /* 初始化工具类功能 */
             if (isInitUtils) {
                 com.blankj.utilcode.util.Utils.init(this);
 
-                //初始化崩溃捕获工具
-                CrashUtils.init();
-
                 //初始化日志工具
-                LogUtils.Config config = LogUtils.getConfig()
+                config = LogUtils.getConfig()
                         // 设置log总开关，包括输出到控制台和文件，默认开
-                        .setLogSwitch(BuildConfig.DEBUG)
+                        .setLogSwitch(AppUtils.isAppDebug())
                         // 设置是否输出到控制台开关，默认开
-                        .setConsoleSwitch(BuildConfig.DEBUG)
+                        .setConsoleSwitch(AppUtils.isAppDebug())
                         // 设置log全局标签，默认为空。当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
-                        .setGlobalTag(null)
+                        .setGlobalTag("HandyBase")
                         // 设置log头信息开关，默认为开
                         .setLogHeadSwitch(true)
                         // 打印log时是否存到文件的开关，默认关
-                        .setLog2FileSwitch(false)
+                        .setLog2FileSwitch(AppUtils.isAppDebug())
                         // 当自定义路径为空时，写入应用的/cache/log/目录中
                         .setDir("")
                         // 当文件前缀为空时，默认为"util"，即写入文件为"util-MM-dd.txt"
                         .setFilePrefix("")
                         // 输出日志是否带边框开关，默认开
-                        .setBorderSwitch(true)
+                        .setBorderSwitch(false)
                         // log的控制台过滤器，和logcat过滤器同理，默认Verbose
                         .setConsoleFilter(LogUtils.V)
                         // log文件过滤器，和logcat过滤器同理，默认Verbose
