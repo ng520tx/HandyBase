b12e46f3e960fafb5d630e370e9ce8f848ee36e8
liujie <liujie045.china@outlook.com>
2018-01-15 00:55:40 +0800

    HandyCommit

--- Library/src/main/java/com/handy/base/mvp/strong/BaseMvpPresenter.java
+++ Library/src/main/java/com/handy/base/mvp/strong/BaseMvpPresenter.java
@@ -1,11 +1,46 @@
 package com.handy.base.mvp.strong;
 
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
 /**
- * Created by LiuJie on 2016/9/5.
+ * 类名
+ *
+ * @author LiuJie https://github.com/Handy045
+ * @description 请描述类的实现或功能
+ * @date Created in 2018/1/15 上午12:14
+ * @modified By LiuJie
  */
-public interface BaseMvpPresenter {
+public abstract class BaseMvpPresenter implements IMvpPresenter {
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    public void onInitPresenter() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        unDispose();
+        this.mCompositeDisposable = null;
+    }
+
     /**
-     * 第一次启动时加载
+     * 将所有disposable放入,集中处理
      */
-    void onInitPresenter();
+    protected void addDispose(Disposable disposable) {
+        if (mCompositeDisposable == null) {
+            mCompositeDisposable = new CompositeDisposable();
+        }
+        mCompositeDisposable.add(disposable);
+    }
+
+    /**
+     * 保证activity结束时取消所有正在执行的订阅
+     */
+    private void unDispose() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.clear();
+        }
+    }
 }
