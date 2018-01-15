74bc8d6fc5e5d9644115e4271cdf29647d5de5e4
liujie <liujie045.china@outlook.com>
2018-01-15 14:55:48 +0800

    HandyCommit

---
+++ Library/src/main/java/com/handy/base/mvp/BaseMvpPresenter.java
@@ -0,0 +1,51 @@
+package com.handy.base.mvp;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * 类名
+ *
+ * @author LiuJie https://github.com/Handy045
+ * @description 请描述类的实现或功能
+ * @date Created in 2018/1/15 上午10:42
+ * @modified By LiuJie
+ */
+public class BaseMvpPresenter<V extends BaseMvpContract.IMvpView> implements BaseMvpContract.IMvpPresenter<V> {
+
+    protected V iMvpView;
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    public void attachView(V iMvpView) {
+        this.iMvpView = iMvpView;
+    }
+
+    @Override
+    public void detachView() {
+        if (iMvpView != null) {
+            iMvpView = null;
+        }
+    }
+
+    @Override
+    public void addDispose(Disposable disposable) {
+        if (mCompositeDisposable == null) {
+            mCompositeDisposable = new CompositeDisposable();
+        }
+        mCompositeDisposable.add(disposable);
+    }
+
+    @Override
+    public void unDispose() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.clear();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+}
--- Library/src/main/java/com/handy/base/mvp/IMvpActivity.java
+++
@@ -1,8 +0,0 @@
-package com.handy.base.mvp;
-
-/**
- * Created by LiuJie on 2016/9/28.
- */
-public interface IMvpActivity<IFragment extends IMvpFragment> {
-    void setFragment(IFragment iFragment);
-}
