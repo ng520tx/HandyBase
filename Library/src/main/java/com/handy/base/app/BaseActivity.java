e5013c45f868903f6b5c6d8cd901d18eab04396c
liujie <liujie045.china@outlook.com>
2018-01-15 21:53:23 +0800

    HandyCommit

--- Library/src/main/java/com/handy/base/app/BaseActivity.java
+++ Library/src/main/java/com/handy/base/app/BaseActivity.java
@@ -8,13 +8,18 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.blankj.utilcode.util.ScreenUtils;
+import com.handy.base.R;
+import com.handy.base.mvp.BaseMvpContract;
 import com.handy.base.utils.ActivityStackUtils;
 import com.handy.base.utils.PermissionsUtils;
+import com.trello.rxlifecycle2.LifecycleTransformer;
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+import cn.bingoogolapple.swipebacklayout.BGASwipeBackHelper;
 
 /**
  * <pre>
@@ -24,11 +29,7 @@ import com.handy.base.utils.PermissionsUtils;
  *  desc  : Activity基本类
  * </pre>
  */
-public abstract class BaseActivity extends AppCompatActivity implements BaseAppApi.BaseAtyApi {
-    public Context context;
-    public Activity activity;
-    public Application application;
-
+public abstract class BaseActivity<IMvpPresenter extends BaseMvpContract.IMvpPresenter> extends RxAppCompatActivity implements BaseAppApi.BaseAtyApi, BaseMvpContract.IMvpView, BGASwipeBackHelper.Delegate {
     /**
      * 屏幕宽度
      */
@@ -66,11 +67,20 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppA
      */
     public boolean isCheckPermissionsHDB = true;
 
+    public Context context;
+    public Activity activity;
+    public Application application;
+
     public Bundle intentBundle = null;
     public Bundle savedInstanceState = null;
 
+    private BGASwipeBackHelper mSwipeBackHelper = null;
+
+    protected IMvpPresenter iMvpPresenter = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        initSwipeBackFinish();
         super.onCreate(savedInstanceState);
         try {
             this.isAlive = true;
@@ -81,6 +91,9 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppA
             this.screenWidth = ScreenUtils.getScreenWidth();
             this.screenHeight = ScreenUtils.getScreenHeight();
 
+            if (iMvpPresenter != null) {
+                iMvpPresenter.attachView(this);
+            }
             ActivityStackUtils.addActivity(this);
         } catch (Exception e) {
             e.printStackTrace();
@@ -131,6 +144,9 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppA
     protected void onPause() {
         super.onPause();
         if (isFinishing()) {
+            if (iMvpPresenter != null) {
+                iMvpPresenter.detachView();
+            }
             isAlive = false;
             ActivityStackUtils.finishChoiceDesc(this);
         }
@@ -223,5 +239,56 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppA
         } else {
             onPermissionRejectionHDB();
         }
+    }
+
+    /**
+     * 是否支持滑动返回。这里在父类中默认返回 true 来支持滑动返回，如果某个界面不想支持滑动返回则重写该方法返回 false 即可
+     */
+    @Override
+    public boolean isSupportSwipeBack() {
+        return true;
+    }
+
+    /**
+     * 初始化滑动返回。在 super.onCreate(savedInstanceState) 之前调用该方法
+     */
+    private void initSwipeBackFinish() {
+        mSwipeBackHelper = new BGASwipeBackHelper(this, this);
+        // 「必须在 Application 的 onCreate 方法中执行 BGASwipeBackManager.getInstance().init(this) 来初始化滑动返回」
+        // 下面几项可以不配置，这里只是为了讲述接口用法。
+        // 设置滑动返回是否可用。默认值为 true
+        mSwipeBackHelper.setSwipeBackEnable(true);
+        // 设置是否仅仅跟踪左侧边缘的滑动返回。默认值为 true
+        mSwipeBackHelper.setIsOnlyTrackingLeftEdge(false);
+        // 设置是否是微信滑动返回样式。默认值为 true
+        mSwipeBackHelper.setIsWeChatStyle(true);
+        // 设置阴影资源 id。默认值为 R.drawable.bga_sbl_shadow
+        mSwipeBackHelper.setShadowResId(R.drawable.bga_sbl_shadow);
+        // 设置是否显示滑动返回的阴影效果。默认值为 true
+        mSwipeBackHelper.setIsNeedShowShadow(true);
+        // 设置阴影区域的透明度是否根据滑动的距离渐变。默认值为 true
+        mSwipeBackHelper.setIsShadowAlphaGradient(true);
+        // 设置触发释放后自动滑动返回的阈值，默认值为 0.3f
+        mSwipeBackHelper.setSwipeBackThreshold(0.3f);
+    }
+
+    @Override
+    public void onSwipeBackLayoutSlide(float v) {
+
+    }
+
+    @Override
+    public void onSwipeBackLayoutCancel() {
+
+    }
+
+    @Override
+    public void onSwipeBackLayoutExecuted() {
+        mSwipeBackHelper.swipeBackward();
+    }
+
+    @Override
+    public <T> LifecycleTransformer<T> bindToLife() {
+        return this.bindToLifecycle();
     }
 }
