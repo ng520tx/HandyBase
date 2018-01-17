a9c9d60f1b90c0cdfa8d749da864024e4c6ea62e
liujie <liujie045.china@outlook.com>
2018-01-17 10:10:10 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -56,9 +56,9 @@ public abstract class BaseApplication extends Application {
                         // 设置log全局标签，默认为空。当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
                         .setGlobalTag("HandyBase")
                         // 设置log头信息开关，默认为开
-                        .setLogHeadSwitch(true)
+                        .setLogHeadSwitch(false)
                         // 打印log时是否存到文件的开关，默认关
-                        .setLog2FileSwitch(AppUtils.isAppDebug())
+                        .setLog2FileSwitch(false)
                         // 当自定义路径为空时，写入应用的/cache/log/目录中
                         .setDir("")
                         // 当文件前缀为空时，默认为"util"，即写入文件为"util-MM-dd.txt"
