e8785a5e5d915ce0545e3da20605fd799a8b172f
liujie <liujie045.china@outlook.com>
2018-02-01 16:54:34 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseActivity.java
+++ Library/src/main/java/com/handy/base/app/BaseActivity.java
@@ -8,18 +8,20 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.blankj.utilcode.util.ScreenUtils;
 import com.handy.base.R;
+import com.handy.base.rxjava.Lifecycleable;
 import com.handy.base.utils.ActivityStackUtils;
 import com.handy.base.utils.PermissionsUtils;
+import com.trello.rxlifecycle2.android.ActivityEvent;
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
 import cn.bingoogolapple.swipebacklayout.BGASwipeBackHelper;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.Subject;
 
 /**
  * <pre>
@@ -29,7 +31,7 @@ import io.reactivex.disposables.Disposable;
  *  desc  : Activity基本类
  * </pre>
  */
-public abstract class BaseActivity extends AppCompatActivity implements BaseApplicationApi.BaseActivityApi, BaseApplicationApi.BaseRxJavaApi, BGASwipeBackHelper.Delegate {
+public abstract class BaseActivity extends RxAppCompatActivity implements Lifecycleable<ActivityEvent>, BaseApplicationApi.BaseActivityApi, BGASwipeBackHelper.Delegate {
     /**
      * 屏幕宽度
      */
@@ -42,11 +44,6 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
      * 界面视图布局
      */
     public View rootLayout = null;
-
-    /**
-     * Activity的活跃状态
-     */
-    public boolean isAlive = false;
     /**
      * onStart中初始化界面视图
      */
@@ -78,15 +75,15 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
 
     public Bundle intentBundle = null;
     public Bundle savedInstanceState = null;
-    public CompositeDisposable compositeDisposable = null;
 
     public BGASwipeBackHelper mSwipeBackHelper;
+
+    private final BehaviorSubject<ActivityEvent> mLifecycleSubject = BehaviorSubject.create();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
-            this.isAlive = true;
             this.context = this;
             this.activity = this;
             this.application = getApplication();
@@ -106,11 +103,9 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
     protected void onStart() {
         super.onStart();
         /* 初始化Activity接收意图的内容 */
-        if (isInitIntentBundle) {
-            if (getIntent().getExtras() != null && getIntent().getExtras().size() > 0) {
-                this.intentBundle = getIntent().getExtras();
-                initIntentBundle(this.intentBundle);
-            }
+        if (isInitIntentBundle && getIntent() != null) {
+            this.intentBundle = getIntent().getExtras();
+            initIntentBundle(this.intentBundle);
             isInitIntentBundle = false;
         }
         /* 安卓权限扫描 */
@@ -146,11 +141,76 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
     protected void onPause() {
         super.onPause();
         if (isFinishing()) {
-            clearRxDisposable();
-
-            isAlive = false;
+            onFinishing();
             ActivityStackUtils.finishChoiceDesc(this);
         }
+    }
+
+    /**
+     * RxJava 任务生命周期关联绑定
+     */
+    @NonNull
+    @Override
+    public Subject<ActivityEvent> provideLifecycleSubject() {
+        return mLifecycleSubject;
+    }
+
+    /**
+     * 应用权限判断处理
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (!PermissionsUtils.checkDeniedPermissions(activity, true)) {
+                onPermissionSuccessHDB();
+            }
+        } else {
+            onPermissionRejectionHDB();
+        }
+    }
+
+    /**
+     * 初始化滑动返回功能。此功能的启用控制可在 {@link Activity#onCreate(Bundle)} 的 super.onCreate(savedInstanceState) 之前或构造方法中设置全局变量 {@link BaseActivity#isUseSwipeBackFinish}
+     */
+    private void initSwipeBackFinish() {
+        mSwipeBackHelper = new BGASwipeBackHelper(this, new BGASwipeBackHelper.Delegate() {
+            @Override
+            public boolean isSupportSwipeBack() {
+                return isUseSwipeBackFinish;
+            }
+
+            @Override
+            public void onSwipeBackLayoutSlide(float slideOffset) {
+
+            }
+
+            @Override
+            public void onSwipeBackLayoutCancel() {
+
+            }
+
+            @Override
+            public void onSwipeBackLayoutExecuted() {
+                mSwipeBackHelper.swipeBackward();
+            }
+        });
+        // 「必须在 Application 的 onCreate 方法中执行 BGASwipeBackManager.getInstance().init(this) 来初始化滑动返回」
+        // 下面几项可以不配置，这里只是为了讲述接口用法。
+        // 设置滑动返回是否可用。默认值为 true
+        mSwipeBackHelper.setSwipeBackEnable(true);
+        // 设置是否仅仅跟踪左侧边缘的滑动返回。默认值为 true
+        mSwipeBackHelper.setIsOnlyTrackingLeftEdge(true);
+        // 设置是否是微信滑动返回样式。默认值为 true
+        mSwipeBackHelper.setIsWeChatStyle(true);
+        // 设置阴影资源 id。默认值为 R.drawable.bga_sbl_shadow
+        mSwipeBackHelper.setShadowResId(R.drawable.bga_sbl_shadow);
+        // 设置是否显示滑动返回的阴影效果。默认值为 true
+        mSwipeBackHelper.setIsNeedShowShadow(true);
+        // 设置阴影区域的透明度是否根据滑动的距离渐变。默认值为 true
+        mSwipeBackHelper.setIsShadowAlphaGradient(true);
+        // 设置触发释放后自动滑动返回的阈值，默认值为 0.3f
+        mSwipeBackHelper.setSwipeBackThreshold(0.3f);
     }
 
     @Override
@@ -164,7 +224,7 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
     }
 
     @Override
-    public void initIntentBundle(Bundle intentBundle) {
+    public void initIntentBundle(Bundle bundle) {
     }
 
     @Override
@@ -188,6 +248,10 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
 
     @Override
     public void onRequestHDB() {
+    }
+
+    @Override
+    public void onFinishing() {
     }
 
     @Override
@@ -223,82 +287,5 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseAppl
          *     }
          * }
          */
-    }
-
-    @Override
-    public void addRxDisposable(Disposable disposable) {
-        if (compositeDisposable == null) {
-            compositeDisposable = new CompositeDisposable();
-        }
-        compositeDisposable.add(disposable);
-    }
-
-    @Override
-    public void clearRxDisposable() {
-        if (compositeDisposable != null) {
-            compositeDisposable.clear();
-            compositeDisposable = null;
-        }
-    }
-
-    /**
-     * ===================================================================
-     * 应用权限判断处理
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (!PermissionsUtils.checkDeniedPermissions(activity, true)) {
-                onPermissionSuccessHDB();
-            }
-        } else {
-            onPermissionRejectionHDB();
-        }
-    }
-
-    /**
-     * 初始化滑动返回。在 super.onCreate(savedInstanceState) 之前调用该方法
-     */
-    private void initSwipeBackFinish() {
-        mSwipeBackHelper = new BGASwipeBackHelper(this, this);
-        // 「必须在 Application 的 onCreate 方法中执行 BGASwipeBackManager.getInstance().init(this) 来初始化滑动返回」
-        // 下面几项可以不配置，这里只是为了讲述接口用法。
-        // 设置滑动返回是否可用。默认值为 true
-        mSwipeBackHelper.setSwipeBackEnable(true);
-        // 设置是否仅仅跟踪左侧边缘的滑动返回。默认值为 true
-        mSwipeBackHelper.setIsOnlyTrackingLeftEdge(true);
-        // 设置是否是微信滑动返回样式。默认值为 true
-        mSwipeBackHelper.setIsWeChatStyle(true);
-        // 设置阴影资源 id。默认值为 R.drawable.bga_sbl_shadow
-        mSwipeBackHelper.setShadowResId(R.drawable.bga_sbl_shadow);
-        // 设置是否显示滑动返回的阴影效果。默认值为 true
-        mSwipeBackHelper.setIsNeedShowShadow(true);
-        // 设置阴影区域的透明度是否根据滑动的距离渐变。默认值为 true
-        mSwipeBackHelper.setIsShadowAlphaGradient(true);
-        // 设置触发释放后自动滑动返回的阈值，默认值为 0.3f
-        mSwipeBackHelper.setSwipeBackThreshold(0.3f);
-    }
-
-    /**
-     * 是否支持滑动返回。这里在父类中默认返回 true 来支持滑动返回，如果某个界面不想支持滑动返回则重写该方法返回 false 即可
-     */
-    @Override
-    public boolean isSupportSwipeBack() {
-        return isUseSwipeBackFinish;
-    }
-
-
-    @Override
-    public void onSwipeBackLayoutSlide(float v) {
-    }
-
-    @Override
-    public void onSwipeBackLayoutCancel() {
-    }
-
-    @Override
-    public void onSwipeBackLayoutExecuted() {
-        mSwipeBackHelper.swipeBackward();
     }
 }
