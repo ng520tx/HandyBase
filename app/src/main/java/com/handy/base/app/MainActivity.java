41acd9dfcaf75524240f27d5b02e43f0df695aca
liujie <liujie045.china@outlook.com>
2018-02-02 10:30:05 +0800

    Handy Commit

--- app/src/main/java/com/handy/base/app/MainActivity.java
+++ app/src/main/java/com/handy/base/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.handy.base.app.dagger.UserActivity;
 import com.handy.base.utils.IntentUtils;
 
 public class MainActivity extends BaseActivity {
@@ -20,7 +21,7 @@ public class MainActivity extends BaseActivity {
         findViewById(R.id.crashTest).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                IntentUtils.openActivity(activity, SubActivity.class, false);
+                IntentUtils.openActivity(activity, UserActivity.class, false);
             }
         });
     }
