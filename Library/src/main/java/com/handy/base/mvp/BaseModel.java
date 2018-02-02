b78338fc4f6b348fa2ed0deb5f0cb6c40e481f98
liujie <liujie045.china@outlook.com>
2018-02-02 15:02:41 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/mvp/BaseModel.java
+++ Library/src/main/java/com/handy/base/mvp/BaseModel.java
@@ -5,33 +5,22 @@ import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.OnLifecycleEvent;
 
-import com.handy.base.retrofit.IRetrofitManager;
-
 
 /**
  * Mvp框架中 Model层通用基类
  *
  * @author LiuJie https://github.com/Handy045
- * @description 所有Model层均需要实现此接口
+ * @description 所有Model层均需要实现此接口.子类主要用于数据的查询、解析，以及向 {@link BasePresenter} 层输出数据对象或者Oberable（RxJava上游对象）等。
  * @date Created in 2018/2/1 上午11:16
  * @modified By LiuJie
  */
 public class BaseModel implements IModel, LifecycleObserver {
-    /**
-     * 用于管理网络请求层,以及数据缓存层
-     */
-    protected IRetrofitManager retrofitManager;
-
-    public BaseModel(IRetrofitManager retrofitManager) {
-        this.retrofitManager = retrofitManager;
-    }
 
     /**
      * 在框架中 {@link BasePresenter#onDestroy()} 时会默认调用 {@link IModel#onDestroy()}
      */
     @Override
     public void onDestroy() {
-        retrofitManager = null;
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
