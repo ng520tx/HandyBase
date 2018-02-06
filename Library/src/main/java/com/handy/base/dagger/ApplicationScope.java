95a82a3bcfba2f1bc442c546f6f9e52e1ae8dc8c
liujie <liujie045.china@outlook.com>
2018-02-06 11:19:27 +0800

    Handy Commit

---
+++ Library/src/main/java/com/handy/base/dagger/ApplicationScope.java
@@ -0,0 +1,22 @@
+package com.handy.base.dagger;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+import javax.inject.Scope;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 中文名称
+ *
+ * @author LiuJie https://github.com/Handy045
+ * @description 功能描述
+ * @date Created in 2018/2/6 上午11:07
+ * @modified By LiuJie
+ */
+@Scope
+@Documented
+@Retention(RUNTIME)
+public @interface ApplicationScope {
+}
