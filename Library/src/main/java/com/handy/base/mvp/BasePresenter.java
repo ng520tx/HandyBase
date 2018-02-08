64dedba3b9cdc86e3bd4e66695a35d256869333e
liujie <liujie045.china@outlook.com>
2018-02-08 15:46:30 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/mvp/BasePresenter.java
+++ Library/src/main/java/com/handy/base/mvp/BasePresenter.java
@@ -1,25 +1,19 @@
 package com.handy.base.mvp;
 
 import android.app.Activity;
-import android.app.Service;
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.OnLifecycleEvent;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.SupportActivity;
-import android.view.View;
 
 import com.blankj.utilcode.util.ObjectUtils;
-import com.trello.rxlifecycle2.RxLifecycle;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
 
 /**
  * Mvp框架中 Presenter层通用基类
@@ -91,21 +85,8 @@ public class BasePresenter<V extends IView> implements IPresenter, LifecycleObse
         this.mCompositeDisposable = null;
     }
 
-    /**
-     * 只有当 {@code view} 不为 null, 并且 {@code view} 实现了 {@link LifecycleOwner} 时, 此方法才会被调用
-     * 所以当您想在 {@link Service} 以及一些自定义 {@link View} 或自定义类中使用 {@code Presenter} 时
-     * 您也将不能继续使用 {@link OnLifecycleEvent} 绑定生命周期
-     *
-     * @param owner link {@link SupportActivity} and {@link Fragment}
-     */
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     void onDestroy(LifecycleOwner owner) {
-        /**
-         * 注意, 如果在这里调用了 {@link #onDestroy()} 方法, 会出现某些地方引用 {@code model} 或 {@code view} 为 null 的情况
-         * 比如在 {@link RxLifecycle} 终止 {@link Observable} 时, 在 {@link io.reactivex.Observable#doFinally(Action)} 中却引用了 {@code view} 做一些释放资源的操作, 此时会空指针
-         * 或者如果你声明了多个 @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY) 时在其他 @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-         * 中引用了 {@code model} 或 {@code view} 也可能会出现此情况
-         */
         owner.getLifecycle().removeObserver(this);
     }
 
