7025984496c264c238219fc240b93ae36556be5f
liujie <liujie045.china@outlook.com>
2018-05-18 17:10:07 +0800

    Handy Commit

--- Library/src/main/java/com/handy/base/app/BaseApplication.java
+++ Library/src/main/java/com/handy/base/app/BaseApplication.java
@@ -1,8 +1,10 @@
 package com.handy.base.app;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.blankj.utilcode.util.AppUtils;
+import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ObjectUtils;
 import com.handy.base.config.BuglyConfig;
@@ -30,6 +32,7 @@ public abstract class BaseApplication extends Application {
     public boolean isInitLogUtils = true;
     public boolean isUseCuntomCrashUtil = true;
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onCreate() {
         super.onCreate();
@@ -37,48 +40,60 @@ public abstract class BaseApplication extends Application {
             /*初始化工具类*/
             Utils.init(this);
             com.blankj.utilcode.util.Utils.init(this);
+
             /*初始化侧滑返回功能*/
             BGASwipeBackHelper.init(this, null);
+
             /*初始化数据库*/
             FlowManager.init(getApplicationContext());
 
-             /* 初始化崩溃捕获工具 */
+            /*初始化崩溃捕获工具*/
             if (isUseCuntomCrashUtil) {
                 com.handy.base.utils.CrashUtils.init();
             } else {
-                com.blankj.utilcode.util.CrashUtils.init();
+                CrashUtils.init(new CrashUtils.OnCrashListener() {
+                    @Override
+                    public void onCrash(String crashInfo, Throwable e) {
+                        LogUtils.e(crashInfo);
+                        AppUtils.relaunchApp();
+                    }
+                });
             }
 
-            /* 初始化日志工具类功能 */
+            /*初始化日志工具*/
             if (isInitLogUtils) {
-                config = LogUtils.getConfig()
-                        // 设置log总开关，包括输出到控制台和文件，默认开
+                final LogUtils.Config config = LogUtils.getConfig()
+                        // 设置 log 总开关，包括输出到控制台和文件，默认开
                         .setLogSwitch(AppUtils.isAppDebug())
                         // 设置是否输出到控制台开关，默认开
                         .setConsoleSwitch(AppUtils.isAppDebug())
-                        // 设置log全局标签，默认为空。当全局标签不为空时，我们输出的log全部为该tag，为空时，如果传入的tag为空那就显示类名，否则显示tag
+                        // 设置 log 全局标签，默认为空
+                        // 当全局标签不为空时，我们输出的 log 全部为该 tag，为空时，如果传入的 tag 为空那就显示类名，否则显示 tag
                         .setGlobalTag("HandyBase")
-                        // 设置log头信息开关，默认为开
-                        .setLogHeadSwitch(false)
-                        // 打印log时是否存到文件的开关，默认关
+                        // 设置 log 头信息开关，默认为开
+                        .setLogHeadSwitch(true)
+                        // 打印 log 时是否存到文件的开关，默认关
                         .setLog2FileSwitch(false)
                         // 当自定义路径为空时，写入应用的/cache/log/目录中
                         .setDir("")
                         // 当文件前缀为空时，默认为"util"，即写入文件为"util-MM-dd.txt"
                         .setFilePrefix("")
                         // 输出日志是否带边框开关，默认开
-                        .setBorderSwitch(false)
-                        // log的控制台过滤器，和logcat过滤器同理，默认Verbose
+                        .setBorderSwitch(true)
+                        // 一条日志仅输出一条，默认开，为美化 AS 3.1 的 Logcat
+                        .setSingleTagSwitch(true)
+                        // log 的控制台过滤器，和 logcat 过滤器同理，默认 Verbose
                         .setConsoleFilter(LogUtils.V)
-                        // log文件过滤器，和logcat过滤器同理，默认Verbose
+                        // log 文件过滤器，和 logcat 过滤器同理，默认 Verbose
                         .setFileFilter(LogUtils.V)
-                        // log栈深度，默认为1
-                        .setStackDeep(1);
-
+                        // log 栈深度，默认为 1
+                        .setStackDeep(1)
+                        // 设置栈偏移，比如二次封装的话就需要设置，默认为 0
+                        .setStackOffset(0);
                 LogUtils.d(config.toString());
             }
 
-            /* 初始化腾讯Bugly应用分析上报功能 */
+            /*初始化腾讯Bugly应用分析上报功能*/
             if (isUseBugly && ObjectUtils.isNotEmpty(buglyID)) {
                 // 实例化Bugly配置对象
                 buglyConfig = new BuglyConfig(buglyID);
