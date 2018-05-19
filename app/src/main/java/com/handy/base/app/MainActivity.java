1c4b8f9cd48c17a5c5ffdbb3fcb1248afb2395b1
liujie <liujie045.china@outlook.com>
2018-05-19 11:54:37 +0800

    Handy Commit

--- app/src/main/java/com/handy/base/app/MainActivity.java
+++ app/src/main/java/com/handy/base/app/MainActivity.java
@@ -5,8 +5,20 @@ import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.handy.base.utils.IntentUtils;
+import com.handy.base.utils.androidutilcode.CoordinateUtils;
 
 public class MainActivity extends BaseActivity {
+
+    public static void main(String[] args) {
+        System.out.println("wgs84ToBd09: " + CoordinateUtils.wgs84ToBd09(117.0465743849, 38.9439192039)[1] + "," + CoordinateUtils.wgs84ToBd09(117.0465743849, 38.9439192039)[0]);
+        System.out.println("bd09ToWGS84: " + CoordinateUtils.bd09ToWGS84(117.0594433713, 38.9506451571)[1] + "," + CoordinateUtils.bd09ToWGS84(117.0594433713, 38.9506451571)[0]);
+        System.out.println("\n");
+        System.out.println("gcj02ToBd09: " + CoordinateUtils.gcj02ToBd09(117.0528459549, 38.9448994339)[1] + "," + CoordinateUtils.gcj02ToBd09(117.0528459549, 38.9448994339)[0]);
+        System.out.println("bd09ToGcj02: " + CoordinateUtils.bd09ToGcj02(117.0594433713, 38.9506451571)[1] + "," + CoordinateUtils.bd09ToGcj02(117.0594433713, 38.9506451571)[0]);
+        System.out.println("\n");
+        System.out.println("gcj02ToWGS84: " + CoordinateUtils.gcj02ToWGS84(117.0528459549, 38.9448994339)[1] + "," + CoordinateUtils.gcj02ToWGS84(117.0528459549, 38.9448994339)[0]);
+        System.out.println("wgs84ToGcj02: " + CoordinateUtils.wgs84ToGcj02(117.0465743849, 38.9439192039)[1] + "," + CoordinateUtils.wgs84ToGcj02(117.0465743849, 38.9439192039)[0]);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
